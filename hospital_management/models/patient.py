b8ddd14ce80727bd7788a4baa310760257a02ef2
Anjum <anjumnakani.skyscendbs@gmail.com>
2024-04-05 06:46:24 +0700

    [Add Icon] hospital_management: Added Module.

--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -15,13 +15,15 @@ class Patient(models.Model):
     height = fields.Float(string='Height (ft)',help='This field is used to take patient height',digits=(16,3))
     Diseases=fields.Selection(selection=[('high blood pressure','High Blood Pressure'),('diabetes','Diabetes'),('cholera','Cholera'),('heart attack','Heart Attack')],help='This field show the list of diseases')
     hospital_name = fields.Char(string="Hospital name",size=4)
-    # print("Hospital name:", hospital_name)this field take a limited char size=4
+    # print(hospital_name this field take a limited char size=4)
 
 
     email = fields.Char('Email',help='This field is used to take patient email')
     phone_number = fields.Char(string='Phone Number', size=10,help='This field is used to take patient phone number')
     address = fields.Char('Address',help='This field is used to take patient address')
     is_admitted = fields.Boolean(string='Admitted',help='This field is used to check that patient is admitted or not')
+    # print("is_admitted for the patient is admitted or not")
+
     birthdate = fields.Date('Birthdate', index=True,help='This field is used for birthdate of patient')
     notes = fields.Text('Notes',help='This field is used for notes')
     today = date.today()
@@ -32,11 +34,11 @@ class Patient(models.Model):
     timestamp = fields.Date('Timestamp', readonly=True)
     Marital_status = fields.Selection([('married', 'Married'), ('unmarried', 'Unmarried'), ('single', 'Single')], string='Marital Status')
 
-    diagnosis_notes = fields.Html(string='Diagnosis Notes',help='This field is used to take diagnosis notes')
+    diagnosis_notes = fields.Text(string='Diagnosis Notes',help='This field is used to take diagnosis notes')
     checkup_date = fields.Date(string='Checkup Date',help='This field is used to take patient checkup date')
     description = fields.Char('Description',help='This field is used to take description')
     medical_history = fields.Text(string='Medical History',help='This field is used to store patient medical history')
-    website=fields.Char('Enter your url')
+    url = fields.Char('URL')
     sign_in = fields.Float('Sign In')
     password = fields.Char('Password',help='This field is used to take password')
     additional_information = fields.Char('Additional Information',help='This field is used to take additional information')
