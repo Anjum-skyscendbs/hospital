6a70f5f680319b16f55db5dbea239a88d77b9376
Anjum <anjumnakani.skyscendbs@gmail.com>
2024-04-15 07:23:20 +0700

    [Added] hospital_management: Improve the code.

--- hospital_management/models/facility.py
+++ hospital_management/models/facility.py
@@ -5,18 +5,12 @@ class Facility(models.Model):
     _description='facility'
 
 
-patient_name = fields.Char(string='Patient Name', required=True,help='This field is used to take patient name')
-patient_id = fields.Integer(string='Patient ID',help='This field is used to take patient id')
-word_rooms=fields.Selection(selection=[('operating room',' Operating Room'),
-                                                   ('patient room',' Patient Room'),
-                                                   ('icu room',' ICU Room'),
-                                                   ('radiology room',' Radiology Room')])
+    patient_name = fields.Char(string='Patient Name', required=True,help='This field is used to take patient name')
+    patient_id = fields.Integer(string='Patient ID',help='This field is used to take patient id')
+    word_rooms=fields.Selection(selection=[('operating room',' Operating Room'),
+                                                       ('patient room',' Patient Room'),
+                                                       ('icu room',' ICU Room'),
+                                                       ('radiology room',' Radiology Room')])
 
-phone_number = fields.Char(string='Phone Number', size=10, help='This field is used to take patient phone number')
-
-
-# The mapping will help us fetch the activities to be displayed against the student.
-# facility = fields.Many2many('hospital.facility','pat_act_rel','patient_id','facility', limit=2)
- # You can also write the many2many field along with the details of lookup table.
-# Firstly you need to give the name of the model as first attribute.
+    phone_number = fields.Char(string='Phone Number', size=10, help='This field is used to take patient phone number')
 
--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -18,6 +18,12 @@ class Patient(models.Model):
     weight = fields.Float(string='Weight (kg)',help='This field is used to take patient weight',digits=(16,3))
     height = fields.Float(string='Height (ft)',help='This field is used to take patient height',digits=(16,3))
     Diseases=fields.Selection(selection=[('high blood pressure','High Blood Pressure'),('diabetes','Diabetes'),('cholera','Cholera'),('heart attack','Heart Attack')],help='This field show the list of diseases')
+
+    # _order = '<field_name>' or '<field_name> desc'
+    # This will be used to sort the fields with a field in either ascending or descending order
+    _order = 'sequence'
+    sequence = fields.Integer('Sequence')
+
     hospital_name = fields.Char(string="Hospital name",size=4)
     # print(hospital_name this field take a limited char size=4)
 
@@ -46,7 +52,7 @@ class Patient(models.Model):
     sign_in = fields.Float('Sign In')
     password = fields.Char('Password',help='This field is used to take password')
     additional_information = fields.Char('Additional Information',help='This field is used to take additional information')
-    template = fields.Html('Template')
+    # template = fields.Html('Template')
 
     priority = fields.Selection([(str(ele), str(ele)) for ele in range(5)], 'Priority')
     blood_group = fields.Selection([
@@ -79,7 +85,7 @@ class Patient(models.Model):
 
     # 3) this field is uses many2many field for the facility
 
-    facility_ids= fields.Many2many('hospital.facility', 'pat_act_rel', 'patient_id', 'act_id', 'Activities', limit=2)
+    facility_ids= fields.Many2many('hospital.facility', 'pat_fact_rel', 'patient_id', 'fact_id', 'Activities', limit=2)
 
     ref = fields.Reference([('hospital.patient', 'patient'),
                             ('res.users', 'Users'),
